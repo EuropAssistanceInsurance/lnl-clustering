c1cfefe0df80b92ffe774512e27b37de6bc2d0b7
cyauEA <chantelle_yau@europ-assistance.fr>
2020-08-18 21:28:45 +0200

    clusters and elbow

--- 1_code/0_prep.R
+++ 1_code/0_prep.R
@@ -15,6 +15,9 @@ names(test) <- fillColNames
 dat <- train %>%
   dplyr::bind_rows(test)
 
+# Remove df's from memory
+rm(test, train)
+
 # Names of columns
 names(dat)
 
@@ -47,17 +50,72 @@ for (vr in cols) {
 head(finDf)
 
 # Use the above to clean columns
-dat2 <- dat %>% 
-  dplyr::mutate(workclass = dplyr::if_else(workclass == "?", "NA", workclass) %>% eaR::cleanNames(sep = "_"),
-                education = education %>% eaR::cleanNames(sep = "_"),
+dat <- dat %>% 
+  dplyr::mutate(workclass      = dplyr::if_else(workclass == "?", "other", workclass) %>% eaR::cleanNames(sep = "_"),
+                education      = education %>% eaR::cleanNames(sep = "_"),
                 marital_status = marital_status %>% eaR::cleanNames(sep = "_"),
-                occupation = dplyr::if_else(occupation == "?", "NA", occupation) %>% eaR::cleanNames(sep = "_"),
-                relationship = relationship %>% eaR::cleanNames(sep = "_"),
-                race = race %>% eaR::cleanNames(sep = "_"),
-                sex = sex %>% eaR::cleanNames(sep = "_"),
+                occupation     = dplyr::if_else(occupation == "?", "other", occupation) %>% eaR::cleanNames(sep = "_"),
+                relationship   = relationship %>% eaR::cleanNames(sep = "_"),
+                race           = race %>% eaR::cleanNames(sep = "_"),
+                sex            = sex %>% eaR::cleanNames(sep = "_"),
                 native_country = native_country %>% eaR::cleanNames(sep = "_"),
-                native_country = dplyr::if_else(native_country %in% c("", "holand_netherlands"), "other", native_country))
+                native_country = dplyr::if_else(native_country %in% c("", "holand_netherlands"), "other", native_country),
+                income         = income %>% gsub(pattern = "\\.", replacement = ""))
 
-# Stratified sampling
-# Separate data into 20 for cluster, 80 train, 20 test
+# Remove finDf from memory
+rm(finDf)
+
+# Stratified sampling separate data into 20 for cluster, 80 train, 20 test
+set.seed(123)
+splitIndex <- caret::createDataPartition(dat[, native_country],
+                                         p     = 0.2,
+                                         list  = FALSE,
+                                         times = 1)
+clusterDF <- dat[splitIndex,  ]
+modDF     <- dat[-splitIndex, ]
+
+
+# Clustering Scaling/Standardising of variables/ Mean and Standard deviation 
+clusters <- clusterDF %>% 
+  group_by(native_country) %>% 
+  summarise(meanAge     = age %>% mean(na.rm = TRUE),
+            sdAge       = age %>% sd(na.rm = TRUE),
+            meanEdu     = education_num %>% mean(na.rm = TRUE),
+            sdEdu       = education_num %>% sd(na.rm = TRUE),
+            meanWgt     = fnlwgt %>% mean(na.rm = TRUE),
+            sdWgt       = fnlwgt %>% sd(na.rm = TRUE),
+            meanhours   = hours_per_week %>% mean(na.rm = TRUE),
+            sdhours     = hours_per_week %>% sd(na.rm = TRUE),
+            meanCapGain = capital_gain %>% mean(na.rm = TRUE),
+            sdCapGain   = capital_gain %>% sd(na.rm = TRUE),
+            meanCapLoss = capital_loss %>% mean(na.rm = TRUE),
+            sdCapLoss = capital_loss %>% sd(na.rm = TRUE),
+            sexMale     = sum(sex == "male", na.rm = TRUE),
+            sexAll      = n(),
+            incomeLT50  = sum(income == "<=50K", na.rm = TRUE),
+            incomeAll   = n()) %>% 
+  mutate(incomePropLT50 = incomeLT50 / incomeAll,
+         sexMaleProp    = sexMale / sexAll) %>% 
+  select(-c(incomeLT50, incomeAll, sexMale, sexAll)) %>% 
+  as.data.frame()
+
+# Drop categorical variable
+rownames(clusters) <- clusters$native_country
+clusters$native_country <- NULL
+
+# Plot elbow 
+set.seed(123)
+factoextra::fviz_nbclust(clusters, kmeans, method = "gap_stat") +
+  geom_vline(xintercept = 3, linetype = 2)
+factoextra::fviz_nbclust(clusters, hcut, method = "gap_stat") +
+  geom_vline(xintercept = 3, linetype = 2)
+
+
+
+
+
+
+
+
+
 
