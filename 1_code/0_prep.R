2987397777808830ec4528c07caf6f26ce03db26
YAU Chantelle <chantelle_yau@europ-assistance.fr>
2020-08-19 13:43:50 +0200

    creating clusters

--- 1_code/0_prep.R
+++ 1_code/0_prep.R
@@ -8,8 +8,8 @@ test <- data.table::fread("0_data/Census Income/adult.test.csv") %>%
 # Fill column names
 fillColNames <- c("age", "workclass", "fnlwgt", "education", "education_num", "marital_status", "occupation", "relationship", "race", "sex", "capital_gain", "capital_loss", "hours_per_week", "native_country", "income")
 
-names(train) <- fillColNames
-names(test) <- fillColNames
+base::names(train) <- fillColNames
+base::names(test) <- fillColNames
 
 # Join train and test
 dat <- train %>%
@@ -19,25 +19,25 @@ dat <- train %>%
 rm(test, train)
 
 # Names of columns
-names(dat)
+base::names(dat)
 
 # Character columns
 cols <- c("workclass", "education", "marital_status", "occupation", "relationship", "race", "sex", "native_country", "income")
 
 # Loop through columns and get the unique contents and counts
 finRes <- c()
-finDf <- data.frame()
+finDf <- base::data.frame()
 for (vr in cols) {
   tempresult <- dat %>% 
     dplyr::group_by(!!as.name(vr)) %>%
     dplyr::summarise(n = n()) %>%
     dplyr::arrange(-n)
-  if(nrow(tempresult) > nrow(finDf)) {
-    for (ii in 1:(nrow(tempresult) - nrow(finDf)))  {
+  if(nrow(tempresult) > base::nrow(finDf)) {
+    for (ii in 1:(base::nrow(tempresult) - base::nrow(finDf)))  {
       finDf <- finDf %>% dplyr::add_row()
     }
   } else {
-    for (ii in 1:(nrow(finDf) - nrow(tempresult)))  {
+    for (ii in 1:(base::nrow(finDf) - base::nrow(tempresult)))  {
       tempresult <- tempresult %>% dplyr::add_row()
     }
   }
@@ -75,38 +75,83 @@ clusterDF <- dat[splitIndex,  ]
 modDF     <- dat[-splitIndex, ]
 
 
-# Clustering Scaling/Standardising of variables/ Mean and Standard deviation 
-clusters <- clusterDF %>% 
-  group_by(native_country) %>% 
-  summarise(meanAge     = age %>% mean(na.rm = TRUE),
-            sdAge       = age %>% sd(na.rm = TRUE),
-            meanEdu     = education_num %>% mean(na.rm = TRUE),
-            sdEdu       = education_num %>% sd(na.rm = TRUE),
-            meanWgt     = fnlwgt %>% mean(na.rm = TRUE),
-            sdWgt       = fnlwgt %>% sd(na.rm = TRUE),
-            meanhours   = hours_per_week %>% mean(na.rm = TRUE),
-            sdhours     = hours_per_week %>% sd(na.rm = TRUE),
-            meanCapGain = capital_gain %>% mean(na.rm = TRUE),
-            sdCapGain   = capital_gain %>% sd(na.rm = TRUE),
-            meanCapLoss = capital_loss %>% mean(na.rm = TRUE),
-            sdCapLoss = capital_loss %>% sd(na.rm = TRUE),
-            sexMale     = sum(sex == "male", na.rm = TRUE),
-            sexAll      = n(),
-            incomeLT50  = sum(income == "<=50K", na.rm = TRUE),
-            incomeAll   = n()) %>% 
-  mutate(incomePropLT50 = incomeLT50 / incomeAll,
-         sexMaleProp    = sexMale / sexAll) %>% 
-  select(-c(incomeLT50, incomeAll, sexMale, sexAll)) %>% 
-  as.data.frame()
+# Clustering Scaling/Standardising of variables/ Mean and Standard deviation to create info 
+clustPrep <- clusterDF %>% 
+  dplyr::group_by(native_country) %>% 
+  dplyr::summarise(meanAge     = age %>% mean(na.rm = TRUE),
+                   sdAge       = age %>% sd(na.rm = TRUE),
+                   meanEdu     = education_num %>% mean(na.rm = TRUE),
+                   sdEdu       = education_num %>% sd(na.rm = TRUE),
+                   meanWgt     = fnlwgt %>% mean(na.rm = TRUE),
+                   sdWgt       = fnlwgt %>% sd(na.rm = TRUE),
+                   meanhours   = hours_per_week %>% mean(na.rm = TRUE),
+                   sdhours     = hours_per_week %>% sd(na.rm = TRUE),
+                   meanCapGain = capital_gain %>% mean(na.rm = TRUE),
+                   sdCapGain   = capital_gain %>% sd(na.rm = TRUE),
+                   meanCapLoss = capital_loss %>% mean(na.rm = TRUE),
+                   sdCapLoss = capital_loss %>% sd(na.rm = TRUE),
+                   sexMale     = sum(sex == "male", na.rm = TRUE),
+                   sexAll      = n(),
+                   incomeLT50  = sum(income == "<=50K", na.rm = TRUE),
+                   incomeAll   = n()) %>% 
+  dplyr::mutate(incomePropLT50 = incomeLT50 / incomeAll,
+                sexMaleProp    = sexMale / sexAll) %>% 
+  dplyr::select(-c(incomeLT50, incomeAll, sexMale, sexAll)) %>% 
+  base::as.data.frame()
+
+# Keep a copy of countries to prep finClust
+finClust <- clustPrep %>%
+  dplyr::select(native_country)
 
 # Drop categorical variable
-rownames(clusters) <- clusters$native_country
-clusters$native_country <- NULL
+rownames(clustPrep) <- clustPrep$native_country
+clustPrep$native_country <- NULL
 
-# Plot elbow 
+# Plot kmeans using elbow method and within sum of squared to find optimal clusters
+# k-means clustering, is to define clusters such that the total intra-cluster variation 
+# [or total within-cluster sum of square (WSS)] is minimized. 
+# The total WSS measures the compactness of the clustering and we want it to be as small as possible.
 set.seed(123)
-factoextra::fviz_nbclust(clusters, kmeans, method = "gap_stat") +
+factoextra::fviz_nbclust(clustPrep, kmeans, method = "wss") +
   geom_vline(xintercept = 3, linetype = 2)
+# Elbow method looks at the total WSS as a function of the number of clusters: 
+# One should choose a number of clusters so that adding another cluster doesn’t 
+# improve much better the total WSS.
+
+# Build clusters
+clusters <- stats::kmeans(clustPrep, centers = 8)
+
+# Look at the output of kmeans
+str(clusters)
+
+# Extract clusters and join back to clusterDF
+finClust$cluster <- clusters$cluster
+
+# Join back original variables for analysis
+clustAnalysis <- clustPrep
+clustAnalysis$clusters <- clusters$cluster
+rownames(clustAnalysis) <- NULL
+
+# Analysis
+clustAnalysis %>% group_by(clusters) %>% summarise_all(.funs = mean)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 factoextra::fviz_nbclust(clusters, hcut, method = "gap_stat") +
   geom_vline(xintercept = 3, linetype = 2)
 
